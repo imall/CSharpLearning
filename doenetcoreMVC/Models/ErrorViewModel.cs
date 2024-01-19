95ebfe2eb1b8893f4a926018f555480fb14a281e
imall <imall841110@gmail.com>
2024-01-19 10:06:45 +0800

    整理檔案

---
+++ doenetcoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace doenetcoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
